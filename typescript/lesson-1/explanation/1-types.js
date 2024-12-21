7c106d127381edd0d549511e68031555545b3992
marcuscastelo <marcuscastelo@usp.br>
2024-12-21 02:04:59 -0300

    fix: corrige typo

--- typescript/lesson-1/explanation/1-types.js
+++ typescript/lesson-1/explanation/1-types.js
@@ -1,9 +1,9 @@
 import { getFavorites } from "./_apoio";
 
 // Código JS: tipagem dinâmica, impossível de "escolher" o tipo de uma variável.
-let a = 10; // x é do tipo number, mas isso não é explicitamente definido pelo programador.
-a = '10'; // Uma linha abaixo, x já mudou de tipo, sem aviso prévio. Agora é do tipo string.
+let a = 10; // a é do tipo number, mas isso não é explicitamente definido pelo programador.
+a = '10'; // Uma linha abaixo, a já mudou de tipo, sem aviso prévio. Agora é do tipo string.
 
 // Podemos ainda criar uma variável sem inicializá-la, e depois atribuir um valor a ela.
-let b // y é inicializado com o valor undefined, mais tarde, pode ser qualquer coisa.
-b = 10; // y agora é do tipo number.
+let b // b é inicializado com o valor undefined, mais tarde, pode ser qualquer coisa.
+b = 10; // b agora é do tipo number.
